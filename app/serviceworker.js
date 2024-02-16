3e92f0db51671176f0716b16bef4e0358e0fc6a9
Swagyo <98276563+SwagyoOfficial@users.noreply.github.com>
2024-02-16 21:19:07 +0530

    Update serviceworker.js

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -8,7 +8,7 @@ importScripts(
 // Configurations for PWA App
 const config = {
   app: {
-    name: "swagyo-pwa-app",
+    name: "ukg-pwa-app",
     version: "v1",
     precache: "install-time",
     runtime: "run-time",
